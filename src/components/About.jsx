1a3ab2ce283f6c2452e4f330810856be50eab926
Blackscorche <blackscorche@gmail.com>
2024-11-22 23:03:30 +0100

    Fixed layout issue in services page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,18 +10,15 @@ function About() {
                 fontWeight: 'normal',
                 textAlign: 'center',
                 borderRadius: '5px',
+                marginBottom:'3rem',
+                overflow:'hidden',
                 height:'100vh'}}>
             <div class="container">
-                <h2>About ScorcheTech</h2>
+                <h1>About ScorcheTech</h1>
                 <p>
                 <strong>ScorcheTech</strong> is a forward-thinking web development and technology solutions provider, dedicated to creating innovative digital experiences for businesses and individuals. Our mission is to empower brands with modern, efficient, and user-friendly technology, helping them thrive in today’s competitive digital landscape.
                 </p>
-                <p>
-                With expertise in custom website development, e-commerce solutions, web and mobile apps, and UI/UX design, ScorcheTech turns ideas into reality. We prioritize functionality, aesthetics, and scalability to deliver solutions tailored to meet unique needs.
-                </p>
-                <p>
-                At ScorcheTech, we believe in simplicity, creativity, and results-driven solutions. Whether it’s a small personal project or a complex business platform, we’re committed to delivering exceptional quality every step of the way.
-                </p>
+
                 <ul>
                 <li>Cutting-edge technology tailored to your needs.</li>
                 <li>Affordable and scalable solutions for businesses of all sizes.</li>
